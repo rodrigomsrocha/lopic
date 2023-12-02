667c48db2cc14899b4d16961152986afc35acf1d
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-12-01 21:02:25 -0300

    feature: add albun to highlights

--- scripts/album.js
+++ scripts/album.js
@@ -10,23 +10,28 @@ const mainImg = document.querySelector(".main-img");
 const gallery = document.querySelector(".gallery");
 const carousel = document.querySelector(".carousel-content");
 
+const highlightBtn = document.querySelector(".highlight-btn");
+
 window.addEventListener("load", () => {
   getData();
   getImgs();
 });
 
+let albun;
+
 async function getData() {
+  updateHighlightState();
   const res = await fetch(
     `https://lopic-server.rodrigomsrocha.repl.co/albuns/${id}`
   );
-  const data = await res.json();
+  albun = await res.json();
 
-  title.innerText = "Álbum " + data.title;
-  description.innerText = data.description;
-  loc.innerText = data.title;
-  date.innerText = data.date;
-  mainImg.setAttribute("src", `../${data.highlight}`);
-  mainImg.setAttribute("alt", data.title);
+  title.innerText = "Álbum " + albun.title;
+  description.innerText = albun.description;
+  loc.innerText = albun.title;
+  date.innerText = albun.date;
+  mainImg.setAttribute("src", `../${albun.highlight}`);
+  mainImg.setAttribute("alt", albun.title);
 }
 
 async function getImgs() {
@@ -61,3 +66,58 @@ async function getImgs() {
 
   carousel.children[0].classList.add("active");
 }
+
+let isAlbumInHighlights = false;
+let highlightId = null;
+
+async function updateHighlightState() {
+  const res = await fetch(
+    "https://lopic-server.rodrigomsrocha.repl.co/highlights"
+  );
+  const data = await res.json();
+
+  const highlight = data.find((highlight) => {
+    return highlight.albunId === +id;
+  });
+
+  isAlbumInHighlights = !!highlight;
+
+  console.log(isAlbumInHighlights);
+
+  if (isAlbumInHighlights) {
+    highlightId = highlight.id;
+    highlightBtn.innerText = "Remover dos destaques";
+  } else {
+    highlightId = null;
+    highlightBtn.innerText = "Adicionar aos destaques";
+  }
+}
+
+async function handleHighlight() {
+  if (isAlbumInHighlights) {
+    await fetch(
+      `https://lopic-server.rodrigomsrocha.repl.co/highlights/${highlightId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } else {
+    await fetch("https://lopic-server.rodrigomsrocha.repl.co/highlights", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        albunId: +id,
+        description: albun.description,
+        url: albun.highlight,
+      }),
+    });
+  }
+  updateHighlightState();
+}
+
+highlightBtn.addEventListener("click", handleHighlight);
